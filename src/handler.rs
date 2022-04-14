8b0fd5e5f063541abb27ad9f7ddc4037ac35e86b
Jacob Michels <jacob.michels2025@gmail.com>
2022-04-13 22:42:38 -0400

    add basic bot structure and test slash command

---
+++ src/handler.rs
@@ -0,0 +1,82 @@
+use serenity::{
+    async_trait,
+    model::{
+        gateway::Ready,
+        id::GuildId,
+        interactions::{
+            application_command::ApplicationCommandOptionType, Interaction,
+            InteractionApplicationCommandCallbackDataFlags, InteractionResponseType,
+        },
+    },
+    prelude::*,
+};
+
+pub struct DiscordHandler;
+
+#[async_trait]
+impl EventHandler for DiscordHandler {
+    async fn ready(&self, _ctx: Context, _data: Ready) {
+        log::info!("Bot ready");
+    }
+
+    // This runs when the cache object is ready and populated with Guild data
+    async fn cache_ready(&self, ctx: Context, guild_ids: Vec<GuildId>) {
+        log::info!("Cache ready");
+
+        // Here we iterate through the guilds and create slash commands in them
+        for guild_id in guild_ids {
+            let name = guild_id.name(ctx.cache.clone()).await.unwrap();
+            log::info!("Adding slash commands to guild: {}", name);
+            let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+                commands.create_application_command(|command| {
+                    command
+                        .name("test")
+                        .description("a test command")
+                        .create_option(|option| {
+                            option
+                                .name("my_option")
+                                .description("a test option")
+                                .kind(ApplicationCommandOptionType::String)
+                                .required(true)
+                                .add_string_choice("name", "value")
+                        })
+                })
+            })
+            .await;
+
+            log::info!(
+                "Successfully added {} commands to {}",
+                commands.unwrap().len(),
+                name
+            );
+        }
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "test" => "right back at ya",
+                _ => "not implemented",
+            };
+
+            let result = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| {
+                            message
+                                .content(content)
+                                .flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL)
+                        })
+                })
+                .await;
+
+            if let Err(why) = result {
+                log::error!(
+                    "An error occured while sending interaction response: {}",
+                    why
+                )
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serenity::prelude::*;
+use simple_logger::SimpleLogger;
+use std::env;
+
+mod handler;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Enable info level logging for this crate only
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Off)
+        .with_module_level("boom_bot", log::LevelFilter::Info)
+        .init()?;
+
+    // Get config from environment
+    let token = env::var("DISCORD_TOKEN")?;
+    let application_id: u64 = env::var("APP_ID")?.parse()?;
+
+    log::info!("Parsed environment credentials");
+
+    // Build and start the client
+    let mut client = Client::builder(token)
+        .event_handler(handler::DiscordHandler)
+        .application_id(application_id)
+        .await?;
+
+    log::info!("Starting client");
+
+    if let Err(why) = client.start().await {
+        log::error!("Client error: {:?}", why)
+    }
+
+    anyhow::Ok(())
 }
